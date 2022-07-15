1bfc6acba9805ca4c2fec698a9592059e66539d4
j-irion <juliusirion@gmx.de>
2022-07-15 18:27:10 +0200

    Make buttons populate screen

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const screen = document.getElementById("screen");
+
 function add(val1, val2) {
   return val1 + val2;
 }
@@ -17,3 +19,12 @@ function divide(val1, val2) {
 function operate(operator, val1, val2) {
   return operator(val1, val2);
 }
+
+let numberButtons = document.querySelectorAll(".number");
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (screen.textContent == 0) screen.textContent = "";
+    screen.textContent += button.textContent;
+  });
+});
